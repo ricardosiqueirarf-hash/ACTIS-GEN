52e3de48a2d70d84eeb5b71f72c5b92f1f4765c0
lowkanta <ricardosiqueirarf@gmail.com>
2026-09-16 03:50:03 -0300

    feat(android): register Termux Core bridge

--- apps/android/android/app/src/main/java/com/actisgen/mobile/MainActivity.java
+++ apps/android/android/app/src/main/java/com/actisgen/mobile/MainActivity.java
@@ -1,5 +1,13 @@
 package com.actisgen.mobile;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ActisTermuxPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
